b369d0667aa0da0461188ba1ad09e23db1e6cd3d
Yifan <yifanli5@asu.edu>
2018-06-14 07:32:24 -0700

    rewrite

--- src/Nodes/GENodes/GEBuyNode.java
+++ src/Nodes/GENodes/GEBuyNode.java
@@ -8,6 +8,7 @@ import Util.ComponentsEnum;
 import Util.Statics;
 import org.osbot.rs07.api.Bank;
 import org.osbot.rs07.api.GrandExchange;
+import org.osbot.rs07.api.Inventory;
 import org.osbot.rs07.api.Tabs;
 import org.osbot.rs07.api.model.NPC;
 import org.osbot.rs07.script.MethodProvider;
@@ -26,7 +27,10 @@ public class GEBuyNode implements MarkovNodeExecutor.ExecutableNode, GrandExchan
     private GrandExchangePolling polling;
     private boolean doPreventIdleAction = true;
 
-    private boolean offerUpdated, offerFinished;
+    int amtTraded;
+    GrandExchange.Box box;
+
+    private boolean offerUpdated;
 
     public GEBuyNode(Script script, ComponentsEnum buy){
         operations = new GrandExchangeOperations();
@@ -40,11 +44,19 @@ public class GEBuyNode implements MarkovNodeExecutor.ExecutableNode, GrandExchan
     public void onGEUpdate(GrandExchange.Box box) {
         GrandExchange ge = script.getGrandExchange();
         if(ge.getItemId(box) == buy.getPrimaryItemID()){
-            script.log("Buy offer updated");
+            this.box = box;
+            amtTraded = ge.getAmountTraded(box);
+            int totalAmtToTrade = ge.getAmountToTransfer(box);
+            script.log("Buy offer updated, box: " + box.toString() + " has bought " + amtTraded + "/" + totalAmtToTrade + "items \nrecieved by " + this.getClass().getSimpleName());
             offerUpdated = true;
             if(ge.getStatus(box) == GrandExchange.Status.FINISHED_BUY){
-                script.log("Buy offer finished");
-                offerFinished = true;
+                polling.removeObserver(this);
+
+                if(amtTraded == totalAmtToTrade){
+                    script.log("Buy offer finished");
+                } else {
+                    script.log("Offer canceled");
+                }
             }
         }
     }
@@ -58,40 +70,88 @@ public class GEBuyNode implements MarkovNodeExecutor.ExecutableNode, GrandExchan
     @Override
     public int executeNode() throws InterruptedException {
         logNode();
+        Inventory inv = script.getInventory();
         if(withdrawCash()){
-            polling.registerObserver(this);
-            if(isBuyItemPending() || operations.buyItem(buy.getPrimaryItemID(), buy.getGeSearchTerm(), 1000)){
-
-                while(!offerUpdated)
-                    Thread.sleep(1000);
-
-                offerUpdated = false;
-                boolean successfulCollect = false;
-                int attempts = 0;
-                while(!successfulCollect && attempts < 5){
-                    successfulCollect = operations.collectAll();
-                    attempts++;
-                    MethodProvider.sleep(1000);
+            MethodProvider.sleep(1000);
+            if(isBuyItemPending()){
+                script.log("canceling previous buy offer");
+                if(operations.abortOffersWithItem(buy.getPrimaryItemName())){
+                    collect();
                 }
-
             }
-            if(offerFinished){
-                polling.removeObserver(this);
-                offerFinished = false;
+            MethodProvider.sleep(1000);
+            if(inv.getAmount(995) >= 300000){
+                int[] margin = operations.priceCheckItem(buy.getPrimaryItemID(), buy.getGeSearchTerm());
+                offerUpdated = false;
+                polling.registerObserver(this);
+                if(selectBuyOperation(margin)){
+                    int loops = 0;
+                    while(!offerUpdated && amtTraded > 14){
+                        loops++;
+                        Thread.sleep(1000);
+                        script.log("Thread: " + Thread.currentThread().getId() + " is spinlocking in GEBuy");
+                        if(loops > 90){
+                            loops = 0;
+                            increaseOffer();
+                        }
+                    }
+                    script.log("Thread: " + Thread.currentThread().getId() + " has released GEBuy spinlock");
+                    collect();
+                }
             }
         }
         return 1000;
+    }
+
+    private boolean increaseOffer() throws InterruptedException {
+        int prevOffer = script.grandExchange.getItemPrice(box);
+        script.log("increasing offer to " + (prevOffer + 25));
+        if(operations.abortOffersWithItem(buy.getPrimaryItemName())){
+            if(collect()){
+                return operations.buyItem(buy.getPrimaryItemID(), buy.getGeSearchTerm(), prevOffer + 25, 1000);
+            }
+        }
+        return false;
+    }
+
+    private boolean collect() throws InterruptedException {
+        boolean successfulCollect = false;
+        int attempts = 0;
+        while(!successfulCollect && attempts < 5){
+            successfulCollect = operations.collectAll();
+            attempts++;
+            MethodProvider.sleep(1000);
+        }
+        if(!successfulCollect){
+            script.log("error in collection");
+            script.stop(false);
+        }
+        return successfulCollect;
     }
 
     private boolean isBuyItemPending(){
         GrandExchange ge = script.getGrandExchange();
         for (GrandExchange.Box box : GrandExchange.Box.values())
             if(ge.getItemId(box) == buy.getPrimaryItemID()){
-                return ge.getStatus(box) == GrandExchange.Status.COMPLETING_BUY ||
-                        ge.getStatus(box) == GrandExchange.Status.FINISHED_BUY;
+                script.log(ge.getStatus(box));
+                if(ge.getStatus(box) == GrandExchange.Status.PENDING_BUY ||
+                        ge.getStatus(box) == GrandExchange.Status.COMPLETING_BUY ||
+                        ge.getStatus(box) == GrandExchange.Status.FINISHED_BUY){
+                    script.log("buy is pending");
+                    return true;
+                }
+
             }
 
         return false;
+    }
+
+    private boolean selectBuyOperation(int[] margin) throws InterruptedException {
+        if(margin[1] - margin[0] <= 75){
+            return operations.buyItem(buy.getPrimaryItemID(), buy.getGeSearchTerm(), margin[1], 1000);
+        } else {
+            return operations.buyItem(buy.getPrimaryItemID(), buy.getGeSearchTerm(), margin, 1000);
+        }
     }
 
     private boolean withdrawCash() throws InterruptedException {
@@ -105,9 +165,8 @@ public class GEBuyNode implements MarkovNodeExecutor.ExecutableNode, GrandExchan
             }.sleep();
             if(success){
                 if(bank.getAmount(995) > 0){
-                    return bank.withdraw(995, Bank.WITHDRAW_ALL);
+                    return bank.withdraw(995, Bank.WITHDRAW_ALL) && bank.close();
                 }
-                return true;
             }
         }
         return false;
